ca1fe062e64b525f1fe14f10037656a5d23a99c3
Swagnik02 <swagnik1234@gmail.com>
2024-01-14 12:46:23 +0530

    build issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'VibeForge',
       theme: ThemeData(
         textTheme: Theme.of(context).textTheme.apply(
               bodyColor: Colors.white,
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,23 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.deepPurple.shade800.withOpacity(0.8),
+          Colors.deepPurple.shade200.withOpacity(0.8),
+        ],
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+        ),
+        body: Container(),
+      ),
+    );
   }
 }
