cd63e8ae4002ca70d4c77c8f5ca2ae9f4866405a
Swagnik02 <swagnik1234@gmail.com>
2024-01-29 22:26:00 +0530

    fixed authScreen

--- lib/screens/Auth/auth_home.dart
+++ lib/screens/Auth/auth_home.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:vibeforge/common/utils.dart';
+import 'package:vibeforge/models/user_model.dart';
 import 'package:vibeforge/screens/Auth/login_screen.dart';
 import 'package:vibeforge/screens/HomeScreen/home_screen.dart';
+import 'package:vibeforge/services/auth_service.dart';
 
-class AuthHome extends StatelessWidget {
-  const AuthHome({
-    super.key,
-  });
+class AuthHome extends StatefulWidget {
+  const AuthHome({Key? key}) : super(key: key);
+
+  @override
+  _AuthHomeState createState() => _AuthHomeState();
+}
+
+class _AuthHomeState extends State<AuthHome> {
+  late AuthService authService;
+  late Future<User?> loggedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+    authService = AuthService();
+    loggedInUser = authService.getLoggedInUser();
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (GlobalUtils.isloggedIn) {
-      return HomeScreen();
-    } else {
-      return LoginScreen();
-    }
+    return FutureBuilder<User?>(
+      future: loggedInUser,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Return a loading indicator if the future is still in progress
+          return CircularProgressIndicator();
+        } else {
+          // Return the appropriate screen based on login status
+          return snapshot.hasData ? HomeScreen() : LoginScreen();
+        }
+      },
+    );
   }
 }
