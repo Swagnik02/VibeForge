02f1b3377c7bc5a7787198a5220764b383a8fddd
Swagnik02 <swagnik1234@gmail.com>
2025-05-31 11:03:50 +0530

    added workflow

--- lib/screens/BottomNav/Home/home.dart
+++ lib/screens/BottomNav/Home/home.dart
@@ -6,9 +6,7 @@ import 'package:vibeforge/vibeComponents/vibe_song_card.dart';
 import '../../../widgets/widgets.dart';
 
 class Home extends StatelessWidget {
-  const Home({
-    super.key,
-  });
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -49,15 +47,13 @@ class Home extends StatelessWidget {
           ),
           Row(
             children: [
-              Text('Crafted with',
-                  style: TextStyle(fontFamily: Fonts.asul, fontSize: 18)),
-              Icon(
-                Icons.favorite_rounded,
-                color: Colors.red,
-                size: 50,
+              Text(
+                'Crafted with',
+                style: TextStyle(fontFamily: Fonts.asul, fontSize: 18),
               ),
-              Text('for Anusha',
-                  style: TextStyle(fontFamily: Fonts.asul, fontSize: 18)),
+              Icon(Icons.favorite_rounded, color: Colors.red, size: 50),
+              // Text('for Anusha',
+              //     style: TextStyle(fontFamily: Fonts.asul, fontSize: 18)),
             ],
           ),
           SizedBox(height: 200),
@@ -68,9 +64,7 @@ class Home extends StatelessWidget {
 }
 
 class _DiscoverMusic extends StatelessWidget {
-  const _DiscoverMusic({
-    super.key,
-  });
+  const _DiscoverMusic({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -79,37 +73,28 @@ class _DiscoverMusic extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            'Welcome',
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
+          Text('Welcome', style: Theme.of(context).textTheme.bodyLarge),
           const SizedBox(height: 5),
           Text(
             'Enjoy your favourite music',
-            style: Theme.of(context)
-                .textTheme
-                .headlineSmall
-                ?.copyWith(fontWeight: FontWeight.bold),
+            style: Theme.of(
+              context,
+            ).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 20),
 
           // Search Text field
           TextFormField(
-            style: const TextStyle(
-              color: Colors.deepPurple,
-            ),
+            style: const TextStyle(color: Colors.deepPurple),
             decoration: InputDecoration(
               isDense: true,
               filled: true,
               fillColor: Colors.white,
               hintText: 'Search',
-              hintStyle: Theme.of(context)
-                  .textTheme
-                  .bodyMedium!
-                  .copyWith(color: Colors.grey.shade400),
-              prefixIcon: const Icon(
-                Icons.search,
-              ),
+              hintStyle: Theme.of(
+                context,
+              ).textTheme.bodyMedium!.copyWith(color: Colors.grey.shade400),
+              prefixIcon: const Icon(Icons.search),
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(15),
                 borderSide: BorderSide.none,
@@ -123,21 +108,14 @@ class _DiscoverMusic extends StatelessWidget {
 }
 
 class _CreatorsChoice extends StatelessWidget {
-  const _CreatorsChoice({
-    super.key,
-    required this.songs,
-  });
+  const _CreatorsChoice({super.key, required this.songs});
 
   final List<VibeSong> songs;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(
-        left: 20,
-        bottom: 20,
-        top: 20,
-      ),
+      padding: const EdgeInsets.only(left: 20, bottom: 20, top: 20),
       child: Column(
         children: [
           const Padding(
@@ -158,7 +136,7 @@ class _CreatorsChoice extends StatelessWidget {
                 );
               },
             ),
-          )
+          ),
         ],
       ),
     );
@@ -166,10 +144,7 @@ class _CreatorsChoice extends StatelessWidget {
 }
 
 class _Playlists extends StatelessWidget {
-  const _Playlists({
-    super.key,
-    required this.playlists,
-  });
+  const _Playlists({super.key, required this.playlists});
 
   final List<Playlist> playlists;
 
